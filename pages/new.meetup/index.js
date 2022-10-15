36c2f76e2fb3898f494f107f4196943f31462ca1
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-10-15 16:55:58 +0400

    Adding the New Meetup Form

--- pages/new.meetup/index.js
+++ pages/new.meetup/index.js
@@ -1,1 +1,14 @@
 // our-doamin.com/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm'
+
+function NewMeetupPage() {
+	function addMeetupHandler(enteredMeetupData) {
+		console.log(enteredMeetupData);
+	}
+
+	return (
+		<NewMeetupForm onAddMeetup={addMeetupHandler} />
+	)
+}
+
+export default NewMeetupPage
